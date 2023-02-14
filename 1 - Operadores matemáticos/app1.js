aebc401606ca332b7d5227b711afc376e9435bad
Daniel <danieldeandradelopes@gmail.com>
2023-02-14 14:11:52 -0300

    strings and concact

---
+++ 1 - Operadores matemáticos/app1.js
@@ -0,0 +1,20 @@
+// comentários são usados o // para uma linha
+
+/*
+  e dessa maneira comentamos 
+  várias linhas
+  de uma vez
+*/
+// console.log(1); exibe um valor no console do terminal
+
+// somar
+console.log("A soma é: ", 10 + 3);
+
+// subtrair
+console.log("A subtração é: ", 10 - 3);
+
+// dividir
+console.log("A divisão é: ", 10 / 3);
+
+// multiplicar
+console.log("A multiplição é: ", 10 * 3);
---
+++ 2 - Strings e concatenação/app1.js
@@ -0,0 +1,14 @@
+// strings => letras, números, caracters diversos, palavras e textos
+
+console.log("a");
+console.log("10 + 10");
+console.log(10 + 10);
+
+// concatenação => ajuntar, grudar, fundir
+// console.log('A minha idade é ' + 31);
+console.log("A minha idade é " + 31);
+
+// concatenação com template string ou template literals
+console.log(`A minha idade é ${31}`);
+
+console.log(`Essa é uma alternativa ${false}`);

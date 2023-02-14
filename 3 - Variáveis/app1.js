54b3e13a1a110eaf811a17de3cec775cf346709f
Daniel <danieldeandradelopes@gmail.com>
2023-02-14 14:41:53 -0300

    variables

---
+++ 3 - Variáveis/app1.js
@@ -0,0 +1,26 @@
+// variáveis em Javascript => São maneiras de armazenarmos informações
+// para usarmos posteriormente
+
+// TIPO VARIÁVEL NOME DA VARIAVEL VALOR?
+
+// VAR => Maneira antiga variável dentro do Javascript
+// permite que adicionemos um valor nela e posteriormente possamos alterar
+// esse valor
+
+var nome = "Daniel";
+var nome = "Paulo";
+console.log(`Meu nome é:  ${nome}`);
+
+// LET => Adicionar um valor nele e também podemos alterar esse valor
+let nomeLet = "Daniel";
+
+nomeLet = "Pedro";
+console.log(`Meu nome é:  ${nomeLet}`);
+
+// CONST => Adicionamos um valor e não podemos mais substituir esse valor
+const nomeConst = "Daniel";
+console.log(`Meu nome é:  ${nomeConst}`);
+
+let sobrenome;
+sobrenome = "Lopes";
+console.log(`Meu sobrenome é: ${sobrenome}`);

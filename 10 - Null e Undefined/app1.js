8620d63ca7d97bd1a710fb34b9a4f27848890780
Daniel <danieldeandradelopes@gmail.com>
2023-02-15 00:25:44 -0300

    null, undefined, nan

---
+++ 10 - Null e Undefined/app1.js
@@ -0,0 +1,12 @@
+// NULL
+let meuNome = null;
+
+console.log(meuNome);
+meuNome = "Daniel";
+console.log(meuNome);
+
+// UNDEFINED
+let meuSobrenome;
+console.log(meuSobrenome);
+meuSobrenome = "Lopes";
+console.log(meuSobrenome);
---
+++ 11 - Not a Number/app1.js
@@ -0,0 +1,3 @@
+console.log("minhapalavra" / 5);
+
+console.log("10%" * 5);
